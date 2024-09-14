78787575a63f879b8d133e32b28891da12bedd99
vinodpatil2002 <vinodnspatil@gmail.com>
2024-09-14 17:06:50 +0530

    Add buffer polyfill for compatibility with Node.js environment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
 })
